91a1cbe71a1f94e33b3f40543a556d83f3a2082b
Kevin Zhang <kzhang8850@yahoo.com>
2016-03-06 01:21:59 -0500

    MVC pattern is in, lots of bugs

--- chicken.py
+++ chicken.py
@@ -25,20 +25,23 @@ SCREEN_H = 900
 
 
 
-class Cloud(object):
+class Cloud(pygame.sprite.Sprite):
 
-	def __init__(self, window, xpos, ypos):
-		self.cloudObj = pygame.image.load('cloud.png')
+	def __init__(self, xpos, ypos):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.image.load('cloud.png')
 		#self.cloudObj.set_colorkey((255,255,255))
-		self.cloudObj = pygame.transform.scale(self.cloudObj, (100,100))
-		self.cloudObj.convert()
+		self.image = pygame.transform.scale(self.image, (100,100))
+		#self.cloudObj.convert()
 
 		
-		self.window = window
+		
 		self.xpos = xpos
 		self.ypos = ypos
+		self.rect = pygame.Rect(self.xpos, self.ypos, 100, 100)
 		self.yvel = 20
-		self.window.blit(self.cloudObj, (self.xpos, self.ypos))
+		#self.window.blit(self.cloudObj, (self.xpos, self.ypos))
 
 
 	def is_in_range(self):
@@ -46,63 +49,102 @@ class Cloud(object):
 		return self.ypos > -100	
 
 	def update(self):
+		"""
+		Moves the rectangle based on x-vel and y-vel
+		"""
 		#self.window.fill(pygame.Color(135, 206, 250), pygame.Rect(self.xpos, self.ypos, 100,100))
 		
 
-		self.ypos -= self.yvel
-		self.window.blit(self.cloudObj, (self.xpos, self.ypos))
+		#self.ypos -= self.yvel
+		self.rect.move(0, self.yvel)
+		#self.window.blit(self.cloudObj, (self.xpos, self.ypos))
 
 
 
-class Chicken(object):
+class Sky():
+	"""
+	Represents all the clouds in the game
+	"""
 
-	def __init__ (self, window):
+	def __init__(self, model):
+		self.model = model
+		self.clouds = pygame.sprite.Group()
+
+		self.num_clouds = 0
+
+		for i in range(10):
+			self.clouds.add(Cloud(random.randint(0,SCREEN_H), random.randint(0,SCREEN_W)))
+			self.num_clouds += 1
+
+	def update(self):	
+		"""
+		makes the clouds scroll up
+		"""
+		for cloud in self.clouds:
+			if not cloud.is_in_range():
+				clouds.remove(cloud)
+				clouds.add(Cloud(random.randint(0,SCREEN_W), SCREEN_H))
+
+		for cloud in self.clouds:
+			cloud.update()
+
+
+
+
+
+class Chicken(pygame.sprite.Sprite):
+	"""
+	The main character, the Chicken class
+	"""
+
+	def __init__ (self):
+		pygame.sprite.Sprite.__init__(self)
+
 		self.chickenObj = pygame.image.load('chicken.png')
 		self.chickenObj.set_colorkey((255,255,255))
 		self.chickenObj = pygame.transform.scale(self.chickenObj, (100, 100))
-		self.chickenObj.convert()
+		#self.chickenObj.convert()
 		self.chickenFlip = pygame.transform.flip(self.chickenObj, True, False)
-		self.chicken = self.chickenObj
-		self.window = window
+		self.image = self.chickenObj
+		
 		self.xpos = SCREEN_W/2 -50
 		self.ypos = 10
 		self.xvel = 10
 		self.yvel = 10
-		self.window.blit(self.chicken, (self.xpos, self.ypos))
+		self.rect = pygame.Rect(self.xpos, self.ypos, 100, 100)
+	
 
 
-
+	def move(self, xvel, yvel):
+		self.rect.move(xvel, yvel)
+		
 
 	def update(self):
+		"""
+		Moves the chicken
+		"""
+		self.move(self.xvel, self.yvel)
 
 		#self.window.fill(pygame.Color(135, 206, 250), pygame.Rect(self.xpos, self.ypos, 100,100))
-		pressed = pygame.key.get_pressed()
-		if pressed[pygame.K_DOWN] and self.ypos <= SCREEN_H - 100:
-			self.ypos += self.yvel
-		if pressed[pygame.K_UP] and self.ypos >= 0:
-			self.ypos -= self.yvel	
-		if pressed[pygame.K_RIGHT] and self.xpos <= SCREEN_W - 100:
-			self.chicken = self.chickenFlip
-			self.xpos += self.xvel
-		if pressed[pygame.K_LEFT] and self.xpos >= 0:
-			self.chicken = self.chickenObj
-			self.xpos -= self.xvel	
-		if pressed[pygame.K_ESCAPE]:
-			pygame.quit()
-			sys.exit()	
+		
 
-		self.window.blit(self.chicken, (self.xpos, self.ypos))
+		#self.window.blit(self.chicken, (self.xpos, self.ypos))
 
 
 
-class Hawk(object):
+class Hawk(pygame.sprite.Sprite):
+	"""
+	The Hawk Class, the predators
+	"""
 
-	def __init__(self, window, pos, xvel, top_hawk):
-		self.hawk = pygame.image.load('hawk.png')
-		self.hawk.set_colorkey((255,255,255))
-		self.hawk = pygame.transform.scale(self.hawk, (150,150))
-		self.hawk.convert()
-		self.window = window
+	def __init__(self, pos, xvel, top_hawk):
+		pygame.sprite.Sprite.__init__(self)
+
+		self.image = pygame.image.load('hawk.png')
+		self.image.set_colorkey((255,255,255))
+		self.image = pygame.transform.scale(self.image, (150,150))
+		#self.image.convert()
+		
 		if top_hawk:
 			self.ypos = pos
 			if pos == -150 or pos == SCREEN_H:
@@ -120,7 +162,7 @@ class Hawk(object):
 		if  self.xpos > SCREEN_W/2:
 			self.xvel = -1 * xvel
 		else:
-			self.hawk = pygame.transform.flip(self.hawk, True, False)
+			self.image = pygame.transform.flip(self.image, True, False)
 			self.xvel = xvel	
 
 		if self.ypos > SCREEN_H/2:	
@@ -128,105 +170,235 @@ class Hawk(object):
 		else:
 			self.yvel = 7
 
-		self.window.blit(self.hawk, (self.xpos, self.ypos))
+		self.rect = pygame.Rect(self.xpos, self.ypos, 150, 150)	
+
+		#self.window.blit(self.hawk, (self.xpos, self.ypos))
 
 	def is_in_range(self):
+		"""
+		Checks if still within screen boundaries
+		"""
 		return self.xpos <= SCREEN_W +150 and self.xpos >= -150 and self.ypos <= SCREEN_H+150 and self.ypos >= -150	
 
 	def update(self, chicken):
+		"""
+		Updates hawks to chase the chicken
+		"""
+
+
 		x_diff = chicken.xpos - self.xpos
 		y_diff = chicken.ypos - self.ypos
 		vector_mag = math.sqrt(x_diff**2 + y_diff**2) * 5
 		self.xvel = self.xvel + x_diff / vector_mag  
 		self.yvel = self.yvel + y_diff / vector_mag
 
-		self.xpos += self.xvel
-		self.ypos += self.yvel
-		self.window.blit(self.hawk, (self.xpos, self.ypos))
+		self.rect.move(self.xvel, self.yvel)
+		#self.window.blit(self.hawk, (self.xpos, self.ypos))
+
+
+
+class Flock():
+	"""
+	Represents all the Hawks in the game
+	"""
+
+	def __init__(self, model):
+
+		self.model = model
+
+		self.hawkfleet = pygame.sprite.Group()
+
+		self.num_hawks = 0
+
+		for i in range(1):
+			self.hawkfleet.add(Hawk(random.randint(-150,SCREEN_H), random.randint(1, 7), True))
+			self.hawkfleet.add(Hawk(random.randint(-150,SCREEN_W), random.randint(1, 7), False))
+			self.num_hawks += 2
+
+	def update(self, chicken):		
+		"""
+		Updates hawks in the flock, checks if they're still in range
+		"""
+
+
+		for hawk in self.hawkfleet:
+				if not hawk.is_in_range():
+					hawkfleet.remove(hawk)	
+
+					if random.choice([True, False]):
+						hawkfleet.add(Hawk(random.randint(-150,SCREEN_W), random.randint(1,7), True))
+					else:
+						hawkfleet.add(Hawk(random.randint(-150,SCREEN_H), random.randint(1,7), False))
+
+		for hawk in self.hawkfleet:
+			hawk.update(chicken)	
+
+
 
 
 		
-class ChickenModel(object):
-	pass
+class ChickenModel:
+	"""
+	Model for the game
+	"""
+	
+	def __init__(self):
+		self.width = SCREEN_W
+		self.height = SCREEN_H
+
+		self.chicken = Chicken()
+		self.chicken_sprite = pygame.sprite.Group(self.chicken)
 
 
-class ChickenView(object):
-	pass
+		self.hawks = Flock(self)
+
+		self.sky = Sky(self)
+
+	def update(self):
+		"""
+		Updates all the stuff
+		"""
 
 
-class ChickenController(object):
-	pass
+		self.chicken_sprite.update()
+		self.hawks.update(self.chicken)
+		self.sky.update()
+
+	def get_drawables(self):
+		"""
+		Gives a list of things to draw in view
+		"""
+		return [self.chicken_sprite, self.hawks.hawkfleet, self.sky.clouds]
+
+
+
+
+
+class ChickenView:
+	"""
+	View for Game
+	"""
+	def __init__(self, model):
+		pygame.init()
+		self.width = SCREEN_W
+		self.height = SCREEN_H
+		self.model = model
+		self.screen = pygame.display.set_mode((self.width, self.height))
+		pygame.display.set_caption('CHICKENS CHICKENS CHICKENS')
+
+	def draw(self):
+		"""
+		Redraws game windows, fetching drawables from model
+		"""
+		self.screen.fill(pygame.Color(135,206,250))
+
+		drawables = self.model.get_drawables()
+
+		for g in drawables:
+			g.draw(self.screen)	
+
+
+		pygame.display.flip()	
+
+
+
+
+
+class ChickenController:
+	"""
+	Controller for Player
+	"""
+
+	def __init__(self, model):
+		self.model = model
+		self.done = False
+
+	def process_events(self):		
+		"""
+		Manages keypresses
+		"""
+		pygame.event.pump
+
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				self.done = True
+			
+			elif event.type == pygame.KEYDOWN:	
+				k = event.key
+
+				if k == pygame.K_DOWN and self.model.chicken.ypos <= SCREEN_H - 100:
+					self.model.chicken.yvel = 10
+
+				if k == pygame.K_UP and self.model.chicken.ypos >= 0:
+					self.model.chicken.yvel = -10
+
+				if k == pygame.K_RIGHT and self.model.chicken.xpos <= SCREEN_W - 100:
+					self.model.chicken.image = self.model.chicken.chickenFlip
+					self.model.chicken.xvel = 10
+
+				if k == pygame.K_LEFT and self.model.chicken.xpos >= 0:
+					self.model.chicken.image = self.model.chicken.chickenObj
+					self.model.chicken.xvel = -10
+
+				if k == pygame.K_ESCAPE:
+					self.done = True
+
+			elif event.type == pygame.KEYUP:
+				k = event.key
+				if k == pygame.K_DOWN:
+					self.model.chicken.yvel = 0		
+				if k == pygame.K_UP:
+					self.model.chicken.yvel = 0		
+				if k == pygame.K_LEFT:
+					self.model.chicken.xvel = 0		
+				if k == pygame.K_RIGHT:
+					self.model.chicken.xvel = 0				
+
+		return self.done								
+
+
 	
 
 class ChickenMain(object):
+	"""
+	Main Class
+	"""
 
-	 def __init__(self, width = SCREEN_W, height = SCREEN_H):
-	 	self.width = width
-	 	self.height = height
+
+	def __init__(self, width = SCREEN_W, height = SCREEN_H):
+		self.width = width
+		self.height = height
 		self.clock = pygame.time.Clock()
-		self.window = pygame.display.set_mode((self.width,self.height))
-		pygame.display.set_caption('Testing 123')
-		self.window.fill(pygame.Color(135,206,250))
+		self.model = ChickenModel()
+		self.view = ChickenView(self.model)
+		self.controller = ChickenController(self.model)
 
-
-	 def MainLoop(self):
-	 	pygame.init()
-		chicken1 = Chicken(self.window)
-		hawkfleet = []
-		for i in range(1):
-			hawkfleet.append(Hawk(self.window, random.randint(-150,SCREEN_H), random.randint(1, 7), True))
-			hawkfleet.append(Hawk(self.window, random.randint(-150,SCREEN_W), random.randint(1, 7), False))
-
-		cloud_list = []
-		for i in range(10):
-			cloud_list.append(Cloud(self.window, random.randint(0,SCREEN_H), random.randint(0,SCREEN_W)))
+		
+		
+	def MainLoop(self):
+		"""
+		Game Loop
+		"""
 
 		lastGetTicks = pygame.time.get_ticks()
 
 		done = False
-	
+
 
 		while not done:
-
-			self.window.fill(pygame.Color(135,206,250))
-
 
 			t = pygame.time.get_ticks()
 			dt = (t - lastGetTicks) / 1000.0
 			lastGetTicks = t
 
-
-			for event in pygame.event.get():
-				if event.type == QUIT:
-					pygame.quit()
-					sys.exit()
-
-			
-			for i in range(len(cloud_list)):
-				if not cloud_list[i].is_in_range():
-					cloud_list[i] = Cloud(self.window, random.randint(0,SCREEN_W), SCREEN_H)
-
-				cloud_list[i].update()
-
-			for i in range(len(hawkfleet)):
-				if not hawkfleet[i].is_in_range():
-					 if random.choice([True, False]):
-					 	hawkfleet[i] = Hawk(self.window, random.randint(-150,SCREEN_W), random.randint(1,7), True)
-					 else:
-					 	hawkfleet[i] = Hawk(self.window, random.randint(-150,SCREEN_H), random.randint(1,7), False)
-
-				hawkfleet[i].update(chicken1)	
-				
-		
-
-			
-		
-			chicken1.update()	
-
-			pygame.display.update()
-
+			done = self.controller.process_events()
+			self.model.update()
+			self.view.draw()
 
 			self.clock.tick(FRAMERATE)		
+
+		pygame.quit()
+		sys.exit()	
 
 if __name__ == '__main__':
 	MainWindow = ChickenMain()
