22c2f8d4f50ed858f29a2154ccf8ceacd0e43994
Kevin Zhang <kzhang8850@yahoo.com>
2016-03-09 21:34:07 -0500

    Horizon stuff

--- chicken.py
+++ chicken.py
@@ -694,12 +694,60 @@ class ChickenView:
 		else: 
 			self.hiscore = '0'
 		self.hiscore_surf = self.font.render("HIGH: {}".format(self.hiscore), False, BLACK)
+
+	def fill_gradient(self,color, gradient, rect=None, vertical=True, forward=True):
+	    """fill a surface with a gradient pattern
+	    Parameters:
+	    color -> starting color
+	    gradient -> final color
+	    rect -> area to fill; default is surface's rect
+	    vertical -> True=vertical; False=horizontal
+	    forward -> True=forward; False=reverse
+	    
+	    """
+	    if True: 
+	    	rect = self.screen.get_rect()
+
+	    x1 = rect.left
+	    x2 = rect.right
+	    y1 = rect.top
+	    y2 = rect.bottom
+	    if vertical: 
+	    	h = y2-y1
+	    else: 
+	        h = x2-x1
+	    if forward:
+	    	a, b = color, gradient
+	    else:       
+	    	b, a = color, gradient
+	    rate = (
+	        float(b[0]-a[0])/h,
+	        float(b[1]-a[1])/h,
+	        float(b[2]-a[2])/h
+	    )
+	    fn_line = pygame.draw.line
+	    if vertical:
+	        for line in range(y1,y2):
+	            color = (
+	                min(max(a[0]+(rate[0]*(line-y1)),0),255),
+	                min(max(a[1]+(rate[1]*(line-y1)),0),255),
+	                min(max(a[2]+(rate[2]*(line-y1)),0),255)
+	            )
+	            fn_line(self.screen, color, (x1,line), (x2,line))
+	    else:
+	        for col in range(x1,x2):
+	            color = (
+	                min(max(a[0]+(rate[0]*(col-x1)),0),255),
+	                min(max(a[1]+(rate[1]*(col-x1)),0),255),
+	                min(max(a[2]+(rate[2]*(col-x1)),0),255)
+	            )
+	            fn_line(self.screen, color, (col,y1), (col,y2))	
      
 	def draw(self, alive, start):
 		"""
 		Redraws game windows, fetching drawables from model
 		"""
-		self.screen.fill(pygame.Color(135,206,250))
+		self.fill_gradient(pygame.Color(0,34,102), WHITE)
 
 		drawables = self.model.get_drawables(start)
 
